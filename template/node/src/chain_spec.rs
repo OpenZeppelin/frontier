88f976d9db93ef27240cd7711f00c430722c7aa0
Ozgun Ozerk <ozgun@subspace.network>
2024-08-13 14:47:19 +0300

    update to v1.13.0

--- template/node/src/chain_spec.rs
+++ template/node/src/chain_spec.rs
@@ -1,8 +1,9 @@
 use std::{collections::BTreeMap, str::FromStr};
 
 use hex_literal::hex;
+use serde::{Deserialize, Serialize};
 // Substrate
-use sc_chain_spec::{ChainType, Properties};
+use sc_chain_spec::{ChainType, Properties, ChainSpecExtension, ChainSpecGroup};
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_consensus_grandpa::AuthorityId as GrandpaId;
 #[allow(unused_imports)]
@@ -11,20 +12,41 @@ use sp_core::{Pair, Public, H160, U256};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 // Frontier
 use frontier_template_runtime::{
-	AccountId, Balance, RuntimeGenesisConfig, SS58Prefix, Signature, WASM_BINARY,
+	AccountId, Balance, SS58Prefix, Signature, WASM_BINARY,
 };
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
-pub type ChainSpec = sc_service::GenericChainSpec<RuntimeGenesisConfig>;
+pub type ChainSpec = sc_service::GenericChainSpec<Extensions>;
 
 /// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
 	TPublic::Pair::from_string(&format!("//{}", seed), None)
 		.expect("static values are valid; qed")
 		.public()
+}
+
+/// The extensions for the [`ChainSpec`].
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ChainSpecGroup, ChainSpecExtension)]
+pub struct Extensions {
+	/// The relay chain of the Parachain.
+	#[serde(alias = "relayChain", alias = "RelayChain")]
+	pub relay_chain: String,
+	/// The id of the Parachain.
+	#[serde(alias = "paraId", alias = "ParaId")]
+	pub para_id: u32,
+}
+
+impl Default for Extensions {
+	fn default() -> Self {
+		Self {
+			relay_chain: "rococo-local".into(),
+			// You MUST set this to the correct network!
+			para_id: 1000,
+		}
+	}
 }
 
 #[allow(dead_code)]
